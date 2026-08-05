d4d7daa37c7e8fb9f8ef892228f1f9fce6ef97a6
Sasha Gil <sashagil611@gmail.com>
2026-08-04 19:17:13 -0500

    Delete HelloWorld.js

--- HelloWorld.js
+++
@@ -1,9 +0,0 @@
-class main {
-    public static void main(String[] args) {
-
-        System.out.println("Hello World!");
-    }
-}
-//Public means that it can be used pretty much everywhere
-//static refers to explain that the method comes from the class and creating an object is not necessary
-//Void means that it won't give back a result
