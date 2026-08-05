c6afdbd68cd68ae85242bbd0945b9326aa76d821
Sasha Gil <sashagil611@gmail.com>
2026-08-04 19:19:21 -0500

    Create HelloWorld.java with main method
    Added a simple Java program that prints 'Hello World!'

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+class main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
+//Public means that it can be used pretty much everywhere
+//static refers to explain that the method comes from the class and creating an object is not necessary
+//Void means that it won't give back a result
